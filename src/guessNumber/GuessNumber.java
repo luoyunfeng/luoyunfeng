834a6cb3c88e0f4240cb369e0c64089884564568
luoyunfeng0729 <luoyunfeng0729@163.com>
2012-10-28 16:33:39 +0800

    ADD

--- src/guessNumber/GuessNumber.java
+++ src/guessNumber/GuessNumber.java
@@ -9,9 +9,31 @@ public class GuessNumber {
     }
 
     public String validate(String userInput) {
-        if(userInput.equals(randomNumber)){
-            return "4a0b";
+
+        int ANum = getANum(userInput);
+        int BNum = getBNum(userInput)-ANum;
+        return ANum+"a"+BNum+"b";
+    }
+    public int getANum(String userInput){
+        char input[] =  userInput.toCharArray();
+        char random[] = this.randomNumber.toCharArray();
+        int rightNum = 0;
+        for (int i =0;i<input.length;i++){
+            if(input[i]==random[i])
+                rightNum++;
         }
-        return "3a0b";
+        return rightNum;
+    }
+    public int getBNum(String userInput){
+        char input[] =  userInput.toCharArray();
+        char random[] = this.randomNumber.toCharArray();
+        int rightNum = 0;
+        for (int i =0;i<input.length;i++){
+            for (int j=0;j<random.length;j++) {
+                if(input[i]==random[j])
+                    rightNum++;
+            }
+        }
+        return rightNum;
     }
 }
